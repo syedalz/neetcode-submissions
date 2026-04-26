e38e1c7d19e4bccadbb5fea74afe5621e8e4bdc5
syedalz <syedalzd772@gmail.com>
2026-04-26 00:07:37 -0600

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.py
@@ -0,0 +1,9 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+
+        for i in range(len(nums)):
+            if nums.count(nums[i]) > 1:
+                return True
+
+        return False
---
+++ Data Structures & Algorithms/duplicate-integer/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        
+        my_dict = {}
+
+        for i in range(len(nums)):
+            if nums[i] in my_dict.keys():
+                return True
+            else:
+                my_dict[nums[i]] = 0
+
+
+        return False
+            
+
+
+                
+            
